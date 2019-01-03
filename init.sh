627c18b4a4734ba0b10187c1c7257a10b83d8d72
dobrodei08 <dobrodei08@yandex.ru>
2019-01-03 09:46:36 +0000

    Gunicorn

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx stop
+sudo /etc/init.d/nginx start
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo gunicorn restart
 #﻿sudo /etc/init.d/mysql start﻿
