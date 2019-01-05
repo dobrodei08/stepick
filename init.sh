5291b8754e5c68bd2cbec1a2ef715d8194beb2cb
dobrodei08 <dobrodei08@yandex.ru>
2019-01-05 16:30:42 +0000

    Gunicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicorn.conf / etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/nginx reload
-gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &
+gunicorn -b 0.0.0.0:8080 hello:app &
