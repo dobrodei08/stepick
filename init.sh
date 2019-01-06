66fc754dbd38389ce080f86aa3f8e9fb8ab635c9
dobrodei08 <dobrodei08@yandex.ru>
2019-01-06 14:10:56 +0000

    My Django

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:app &
+#gunicorn -b 0.0.0.0:8080 hello:app &
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
