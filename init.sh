3893782443dedfc378ee08a6046cd40a4e6232d4
dobrodei08 <dobrodei08@yandex.ru>
2019-01-06 15:30:48 +0000

    My Django Girls app, first commit

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/nginx restart
 #gunicorn -b 0.0.0.0:8080 hello:app &
+#sudo gunicorn -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn.conf ask.wsgi:application &
+cd ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
