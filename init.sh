77a4ba46cffe1d9662692710dbb5d209efaccd8e
dobrodei08 <dobrodei08@yandex.ru>
2019-01-07 22:06:41 +0000

    Django 2_5

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
-#mysql -uroot -e "create database ask"
-#gunicorn -b 0.0.0.0:8080 hello:app &
+mysql -uroot -e "create database ask"
+gunicorn -b 0.0.0.0:8080 hello:app &
 cd ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
 
